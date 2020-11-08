2c04743c38a3ca3def63e92aecbe08d15a3f7f63
Tony Mountifield <tony@mountifield.org>
2020-11-08 16:10:49 +0000

    Move list of servers to separate file. Allow extra servers.

---
+++ src/servers.js
@@ -0,0 +1,20 @@
+const central = [
+	{ server: "jamulus.fischvolk.de:22124", desc: "Default server" },
+	{ server: "jamulusallgenres.fischvolk.de:22224", desc: "All Genres" },
+	{ server: "jamulusrock.fischvolk.de:22424", desc: "Genre Rock" },
+	{ server: "jamulusjazz.fischvolk.de:22324", desc: "Genre Jazz" },
+	{ server: "jamulusclassical.fischvolk.de:22524", desc: "Genre Classical/Folk/Choir" },
+	{ server: "worldjam.vip:22124", desc: "Worldjam server" },
+];
+
+let extra = [
+];
+
+let single = [
+];
+
+export default {
+	central,
+	extra,
+	single
+};
