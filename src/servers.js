c456784f25d95219d26aea1e4c3cf761bba420c8
Tony Mountifield <tony@mountifield.org>
2021-01-25 20:42:39 +0000

    Update for new central servers.

--- src/servers.js
+++ src/servers.js
@@ -1,9 +1,11 @@
 const central = [
-	{ server: "jamulus.fischvolk.de:22124", desc: "Default server" },
-	{ server: "jamulusallgenres.fischvolk.de:22224", desc: "All Genres" },
+	{ server: "jamulus.fischvolk.de:22124", desc: "Any Genre 1 (was Default)" },
+	{ server: "jamulusallgenres.fischvolk.de:22224", desc: "Any Genre 2 (was All Genres)" },
+	{ server: "jamulusanygenre3.fischvolk.de:22624", desc: "Any Genre 3" },
 	{ server: "jamulusrock.fischvolk.de:22424", desc: "Genre Rock" },
 	{ server: "jamulusjazz.fischvolk.de:22324", desc: "Genre Jazz" },
-	{ server: "jamulusclassical.fischvolk.de:22524", desc: "Genre Classical/Folk/Choir" },
+	{ server: "jamulusclassical.fischvolk.de:22524", desc: "Genre Classical/Folk" },
+	{ server: "jamuluschoral.fischvolk.de:22724", desc: "Genre Choral/Barbershop" },
 	{ server: "worldjam.vip:22124", desc: "Worldjam server" },
 ];
 
