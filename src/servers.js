375d092e3a283828e28cb12063827bbac7228f26
Tony Mountifield <tony@mountifield.org>
2021-03-12 11:53:36 +0000

    Update central server addresses.

--- src/servers.js
+++ src/servers.js
@@ -1,11 +1,11 @@
 const central = [
-	{ server: "jamulus.fischvolk.de:22124", desc: "Any Genre 1 (was Default)" },
-	{ server: "jamulusallgenres.fischvolk.de:22224", desc: "Any Genre 2 (was All Genres)" },
-	{ server: "jamulusanygenre3.fischvolk.de:22624", desc: "Any Genre 3 (new)" },
-	{ server: "jamulusrock.fischvolk.de:22424", desc: "Genre Rock" },
-	{ server: "jamulusjazz.fischvolk.de:22324", desc: "Genre Jazz" },
-	{ server: "jamulusclassical.fischvolk.de:22524", desc: "Genre Classical/Folk" },
-	{ server: "jamuluschoral.fischvolk.de:22724", desc: "Genre Choral/Barbershop (new)" },
+	{ server: "anygenre1.jamulus.io:22124", desc: "Any Genre 1 (was Default)" },
+	{ server: "anygenre2.jamulus.io:22224", desc: "Any Genre 2 (was All Genres)" },
+	{ server: "anygenre3.jamulus.io:22624", desc: "Any Genre 3 (new)" },
+	{ server: "rock.jamulus.io:22424", desc: "Genre Rock" },
+	{ server: "jazz.jamulus.io:22324", desc: "Genre Jazz" },
+	{ server: "classical.jamulus.io:22524", desc: "Genre Classical/Folk" },
+	{ server: "choral.jamulus.io:22724", desc: "Genre Choral/Barbershop (new)" },
 	{ server: "worldjam.vip:22124", desc: "Worldjam server" },
 ];
 
