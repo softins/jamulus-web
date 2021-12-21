127823e70bb07112887d9bc4ec69ed488e19ef34
Tony Mountifield <tony@mountifield.org>
2021-12-21 16:18:58 +0000

    Update directory names to remove old clarification

--- src/servers.js
+++ src/servers.js
@@ -1,11 +1,11 @@
 const directory = [
-	{ server: "anygenre1.jamulus.io:22124", desc: "Any Genre 1 (was Default)" },
-	{ server: "anygenre2.jamulus.io:22224", desc: "Any Genre 2 (was All Genres)" },
-	{ server: "anygenre3.jamulus.io:22624", desc: "Any Genre 3 (new)" },
+	{ server: "anygenre1.jamulus.io:22124", desc: "Any Genre 1" },
+	{ server: "anygenre2.jamulus.io:22224", desc: "Any Genre 2" },
+	{ server: "anygenre3.jamulus.io:22624", desc: "Any Genre 3" },
 	{ server: "rock.jamulus.io:22424", desc: "Genre Rock" },
 	{ server: "jazz.jamulus.io:22324", desc: "Genre Jazz" },
 	{ server: "classical.jamulus.io:22524", desc: "Genre Classical/Folk" },
-	{ server: "choral.jamulus.io:22724", desc: "Genre Choral/Barbershop (new)" },
+	{ server: "choral.jamulus.io:22724", desc: "Genre Choral/Barbershop" },
 	{ server: "worldjam.vip:22124", desc: "Worldjam server" },
 ];
 
