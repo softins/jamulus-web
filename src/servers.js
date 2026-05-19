ab77429c14e363cfa2cf5f467c059461e74cf4e5
Tony Mountifield <tony@mountifield.org>
2026-05-19 16:08:36 +0100

    Change Any Genre 3 to Any Genre Asia

--- src/servers.js
+++ src/servers.js
@@ -1,7 +1,7 @@
 const directory = [
 	{ server: "anygenre1.jamulus.io:22124", desc: "Any Genre 1" },
 	{ server: "anygenre2.jamulus.io:22224", desc: "Any Genre 2" },
-	{ server: "anygenre3.jamulus.io:22624", desc: "Any Genre 3" },
+	{ server: "asia.jamulus.io:22624", desc: "Any Genre Asia" },
 	{ server: "rock.jamulus.io:22424", desc: "Genre Rock" },
 	{ server: "jazz.jamulus.io:22324", desc: "Genre Jazz" },
 	{ server: "classical.jamulus.io:22524", desc: "Genre Classical/Folk" },
