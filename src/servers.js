c589ad6f8aaa9f6f168827bd660b7f6351909f37
Tony Mountifield <tony@mountifield.org>
2026-05-21 16:19:03 +0100

    Change from jamulus.io to jamulus.app

--- src/servers.js
+++ src/servers.js
@@ -1,16 +1,16 @@
 const directory = [
-	{ server: "anygenre1.jamulus.io:22124", desc: "Any Genre 1" },
-	{ server: "anygenre2.jamulus.io:22224", desc: "Any Genre 2" },
-	{ server: "asia.jamulus.io:22624", desc: "Any Genre Asia" },
-	{ server: "rock.jamulus.io:22424", desc: "Genre Rock" },
-	{ server: "jazz.jamulus.io:22324", desc: "Genre Jazz" },
-	{ server: "classical.jamulus.io:22524", desc: "Genre Classical/Folk" },
-	{ server: "choral.jamulus.io:22724", desc: "Genre Choral/Barbershop" },
+	{ server: "anygenre1.jamulus.app:22124", desc: "Any Genre 1" },
+	{ server: "anygenre2.jamulus.app:22224", desc: "Any Genre 2" },
+	{ server: "asia.jamulus.app:22624", desc: "Any Genre Asia" },
+	{ server: "rock.jamulus.app:22424", desc: "Genre Rock" },
+	{ server: "jazz.jamulus.app:22324", desc: "Genre Jazz" },
+	{ server: "classical.jamulus.app:22524", desc: "Genre Classical/Folk" },
+	{ server: "choral.jamulus.app:22724", desc: "Genre Choral/Barbershop" },
 	// { server: "worldjam.vip:22124", desc: "WorldJam" },
 ];
 
 const notes = {
-	"asia.jamulus.io:22624": "This directory is named \"Any Genre 3\" in Jamulus version 3.12.1 and earlier"
+	"asia.jamulus.app:22624": "This directory is named \"Any Genre 3\" in Jamulus version 3.12.1 and earlier"
 };
 
 let extra = [
